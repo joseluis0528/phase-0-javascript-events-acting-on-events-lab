b8d051f33cd7eb6a049bfee49cc12c48a0bfdb33
Jose <joseluis0528@gmail.com>
2023-03-12 15:14:44 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left - 5}px`;
+    }
+  }
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("360px", "");
+    const right = parseInt(rightNumbers, 10)
+    if (right > 0) {
+        dodger.style.left = `${right + 5}px`
+    }
+  }
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  })
